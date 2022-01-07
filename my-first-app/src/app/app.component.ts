b2de7a52e8d51ff6914538065745bc9d8c9280c1
m-waqas-baqir <82137320+m-waqas-baqir@users.noreply.github.com>
2022-01-07 12:34:18 +0500

    basics

---
+++ my-first-app/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+}
+
+// Component is actually converted to the byta version of javascript 
